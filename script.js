c19ba97af8b7d009a1148d9a5f56b04aea6a2cbc
Othmane ESSAHABY <othmane.essahaby@gmail.com>
2020-05-26 15:19:22 +0000

    lastUpdate

--- script.js
+++ script.js
@@ -1,39 +1,64 @@
 
 $(document).ready(function(){
-  swal({
-    title: "General Precautions",
-    text: "Don't Touch Your mo",
-    icon: "warning",
-    buttons: true,
-    dangerMode: true,
-  })
-  .then((ok1) => {
-    if (ok1) {
-      swal({
-        title: "General Precautions",
-        text: "Wach your hands evry time",
-        icon: "success",
-        buttons: true,
-        dangerMode: true,
+  //******************************************* SWEEET ALERT****************** */
+  Swal.fire({
+    title: 'General',
+    html: 'There is currently no vaccine to prevent <b style="color:red">COVID-19.</b><br>'+
+    'The virus is thought to be spread mainly through person-to-person contact through respiratory droplets produced when an infected person coughs or sneezes.',
+    imageUrl: './img/image1.jpg',
+    imageWidth: 800,
+    imageHeight: 200,
+    customClass: 'swal-wide',
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire({
+        title: 'Washing your hands',
+        html: 'Wash hands frequently with soap and water for at least 20 seconds. Always wear a hand sanitizer that contains at least 60% alcohol. Avoid touching your eyes, nose and mouth with unwashed hands.',
+        imageUrl: './img/image2.jpg',
+        imageWidth: 800,
+        imageHeight: 200,
+        customClass: 'swal-wide',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire({
+          title: 'Protect others from getting sick',
+          html: 'Avoid close contact with sick people. Distance yourself from others if COVID-19/Coronavirus spreads in your community.',
+          imageUrl: './img/image3.jpg',
+          imageWidth: 800,
+          imageHeight: 200,
+          customClass: 'swal-wide',
+      }).then((result) => {
+        if (result.value) {
+          Swal.fire({
+            title: 'Protect others from getting sick',
+            html: 'Cover your mouth and nose with a tissue when you cough or sneeze or use the inside of your elbow. Wash your hands immediately with soap and water for at least 20 seconds or clean your hands with a hand sanitizer that contains at least 60% alcohol.',
+            imageUrl: './img/image4.jpg',
+            imageWidth: 800,
+            imageHeight: 200,
+            customClass: 'swal-wide',
+        })
+      }
       })
-    } else {
-      swal("Welcome to your website");
     }
-  });
+    })
+  }
+  })
   var data_affected = Array();
   const url = 'https://pomber.github.io/covid19/timeseries.json';
   $.getJSON(url, function (data) {
-    var today = new Date();
+  //********************** Definir les nombres de jours passant  *****************/
+  var today = new Date();
   var a = data["Italy"][0]["date"].split("-");
   var date1 = new Date(a);
   
   const diffDays = Math.abs(today.getDate() - date1.getDate())+Math.abs(30*(today.getMonth()-date1.getMonth()))+ Math.abs(365*(today.getFullYear() - date1.getFullYear()));
   document.getElementById("days").innerHTML = diffDays+'  days';
-  startTime();
-
-    document.getElementById("lastupdate").innerHTML = 'Last Update '+data["US"][data["US"].length-1]["date"];
+  startTime();// C'est une fonction donnant le temps exacte 
 
 
+  document.getElementById("lastupdate").innerHTML = 'Last Update '+data["US"][data["US"].length-1]["date"];// le dernier date de modification
+
+/**********************Definir la map********* */
     am4core.ready(function() {
     
       // Themes begin
@@ -55,13 +80,7 @@ $(document).ready(function(){
       polygonSeries.exclude = ["AQ"];
 
       // This array will be populated with country IDs to exclude from the world series
-      var excludedCountries = ["AQ"];
-
-    
-
-
-
-      
+      var excludedCountries = ["AQ"];  
       // Make map load polygon (like country names) data from GeoJSON
       polygonSeries.useGeodata = true;
       
@@ -72,7 +91,7 @@ $(document).ready(function(){
       var array_date_ev = Array();
       var polygonTemplate = polygonSeries.mapPolygons.template;
       
-      
+      //*****************Fonction afficher la graphe line lorsque on click sur un pays dans la map */
       polygonTemplate.events.on("hit", function(ev) {
         try{
           $('html,body').animate({
@@ -86,10 +105,7 @@ $(document).ready(function(){
         array_deaths_ev =[];
         array_recovered_ev =[];
         array_date_ev =[];
-        
-       // remp3 ='<h2 >'+ev.target.dataItem.dataContext.name+'</h2><div style="height:400px;overflow:auto;"><table class="table table-hover table-dark" id="table2" style="text-align: center; border: 2px solid white; margin-right:100px;"><thead ><tr><th scope="col"style="color:aqua;font-size:14px;">CONFIRMED</th><th scope="col"style="color:aqua;font-size:14px;">DEATHS</th><th scope="col"style="color:aqua;font-size:14px;">RECOVERED</th><th scope="col" style="color:aqua; font-size:14px;">DATE</th></tr></thead><tbody id="tbody" >';
-        //data[ev.target.dataItem.dataContext.name].forEach(element=>{
-          
+          /**************L'ajout des donnees (deaths confirmed ..) pour chaque date*********** */
           for (var i=0;i< data[ev.target.dataItem.dataContext.name].length;i++){
             array_confirmed_ev.push(data[ev.target.dataItem.dataContext.name][i]["confirmed"]);
             array_deaths_ev.push(data[ev.target.dataItem.dataContext.name][i]["deaths"]);
@@ -98,12 +114,7 @@ $(document).ready(function(){
 
   
         }
-
- 
-          
-        //})
-  
-  
+/*****************Tracer le graphe ************ */
         new Chart(document.getElementById("chart"), {
           type: 'line',
           data: {
@@ -147,13 +158,12 @@ $(document).ready(function(){
 
         }catch(error){
           alert(ev.target.dataItem.dataContext.name + ' does not have any statics yet');
-          //window.location.href = 'index.html'
         }
       
       });
 
 
-      polygonTemplate.tooltipText = "{name}: {pays_hover}";
+      polygonTemplate.tooltipText = "{name}";
       polygonTemplate.polygon.fillOpacity = 0.6;
       
       
@@ -189,30 +199,19 @@ var x;
 var guerie = 0;
 var confirme = 0;
 var mortes = 0;
-    for(var i=data["Morocco"].length-1; i>=data["Morocco"].length-35;i++){
-   
-        label_date = data["Morocco"][i]["date"];
-        labels.unshift(label_date)
-       i-=7;
-    }
-
    var op ='&nbsp;&nbsp;&nbsp;&nbsp<input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()"><br><ul class="list-group">';
   
   $.each(data, function (key, entry) {
-  
+    
+    op += '&nbsp;&nbsp;&nbsp;&nbsp;<li list-group-item><input type="checkbox" id="'+key+'" value="'+key+'" name=type class="check"><label for="'+key+'">'+key+'</label></li>';
+    /***Remplir les donnees pour les stocker dans firebase ********** */
     guerie = guerie + data[key][data[key].length -1]["recovered"];
     mortes = mortes +  data[key][data[key].length -1]["deaths"];
     confirme = confirme + data[key][data[key].length -1]["confirmed"];;
     var d = data[key][data[key].length -1]["confirmed"];
     var b = data[key][data[key].length -1]["recovered"];
     var c = data[key][data[key].length -1]["deaths"];
-
-      a.push(key)
-
-
-    //dropdown.append($('<option id="op"></option>').attr('value','').text(key));
-    
-    op += '&nbsp;&nbsp;&nbsp;&nbsp;<li list-group-item><input type="checkbox" id="'+key+'" value="'+key+'" name=type class="check"><label for="'+key+'">'+key+'</label></li>';
+    a.push(key)
     x = currentdate+'/'+ key
     const doc = Docref.doc(x)
     doc.set({
@@ -227,8 +226,17 @@ var mortes = 0;
     
    
   })
-  op += '</ul>';
- 
+  op += '</ul>';//fermer la ul lors de la fin de la boucle
+  $("#country").html(op); // ajouter les checkboxes et les different pays contaminees au DropDown multiple select
+  /**************Remplire les donnees  **************** */
+  document.getElementById("hco").innerHTML= confirme;//pour Confirmed 
+  document.getElementById("hde").innerHTML =mortes;//pour deaths
+  document.getElementById("hre").innerHTML = guerie;//pour recorved
+  /************Remplire les donnees et la generalite ********** */
+  document.getElementById("Recovery_Rate").innerHTML = ((guerie/(mortes+guerie+confirme))*100).toFixed(2)+' %';
+  document.getElementById("Death_Rate").innerHTML = ((mortes/(mortes+guerie+confirme))*100).toFixed(2)+' %';
+  document.getElementById("Confirmed_Rate").innerHTML = ((confirme/(mortes+guerie+confirme))*100).toFixed(2)+' %';
+  /************Datatable **************** */
   $('#table').DataTable({
     ajax: { 
     url :'https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search?limit=200',
@@ -252,16 +260,6 @@ var mortes = 0;
    ]
  });
 
- 
-  
-  $("#country").html(op);
-
-document.getElementById("hco").innerHTML= confirme;
-document.getElementById("hde").innerHTML =mortes;
-document.getElementById("hre").innerHTML = guerie;
-document.getElementById("Recovery_Rate").innerHTML = ((guerie/(mortes+guerie+confirme))*100).toFixed(2)+' %';
-document.getElementById("Death_Rate").innerHTML = ((mortes/(mortes+guerie+confirme))*100).toFixed(2)+' %';
-document.getElementById("Confirmed_Rate").innerHTML = ((confirme/(mortes+guerie+confirme))*100).toFixed(2)+' %';
 
  
   var date_array = Array();
@@ -647,15 +645,11 @@ function checkTime(i) {
   if (i < 10) {i = "0" + i};
   return i;
 }
-
- 
-
+/*************LORSQU ON CLICK SUR LE BUTTON SUBMIT  ************* */
 $("#buttonSumbit").click(function(){
 
-  
   favorite =[];
   $('#div3').html('');
-  //remp1 ='<h2 id ="h2"></h2><div style="height:400px;overflow:auto;"><table class="table table-hover table-dark" id="table2" style="text-align: center; border: 2px solid white; margin-right:100px;"><thead ><tr><th scope="col"style="color:aqua;font-size:14px;">CONFIRMED</th><th scope="col"style="color:aqua;font-size:14px;">DEATHS</th><th scope="col"style="color:aqua;font-size:14px;">RECOVERED</th><th scope="col" style="color:aqua; font-size:14px;">DATE</th></tr></thead><tbody id="tbody" >';
    total_array_confirmed = new Array();
    total_array_deaths = new Array();
    total_array_recovered = new Array();
@@ -664,17 +658,15 @@ $("#buttonSumbit").click(function(){
   array_deaths1 =new Array();
   array_recovered1 =new Array();
   array_date = new Array();
+  /**********POUR CHAQUE CHECKBOOX SELECTED ON AJOUTE LES PAYS SELECTIONNES DANS ARRAY FAVORITE*********** */
 $.each($("input[name='type']:checked"), function(){
     favorite.push($(this).val());
 });
+/**********SI ON N AJOUTE RIEN *********** */
 if(favorite.length == 0){
   alert('Plaease choose country')
 
 }else{
-
-
-
-
 
 const url = 'https://pomber.github.io/covid19/timeseries.json';
 document.getElementById("div1").style.display = 'none';
